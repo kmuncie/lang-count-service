6afe39372e49d5a832ca9f9c75e3e607adf0aefe
Kevin Eugene Muncie <nivek2002@gmail.com>
2018-10-22 19:56:08 -0400

    Update json with date and guid and better wording

--- handler.ts
+++ handler.ts
@@ -1,14 +1,25 @@
 import { APIGatewayEvent, Callback, Context, Handler } from 'aws-lambda';
 
 export const langCount: Handler = (event: APIGatewayEvent, context: Context, cb: Callback) => {
+
+   const now = new Date().toISOString();
+
+   /** https://stackoverflow.com/a/2117523 */
+   function uuidv4() {
+      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+         var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+         return v.toString(16);
+      });
+   }
+
    const response = {
       statusCode: 200,
       body: JSON.stringify({
-         uid: 'urn:uuid:1335c695-cfb8-4ebb-abbd-80da344efa6b',
-         updateDate: '2016-05-23T00:00:00.0Z',
-         titleText: 'Amazon Developer Blog, week in review May 23rd',
-         mainText: 'Meet Echosim. A new online community tool for developers that simulates the look and feel of an Amazon Echo.',
-         redirectionUrl: 'https://developer.amazon.com/public/community/blog',
+         uid: uuidv4(),
+         updateDate: now,
+         titleText: 'Number of languages currently supported on JW.ORG',
+         mainText: 'Today JW dot org supports ' + '983' + ' langauges.',
+         redirectionUrl: 'https://jw.org/',
       }),
    };
 
