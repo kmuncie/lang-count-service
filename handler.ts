4fe98b482951e0b1182291c12fae11cc026f66e7
Kevin Eugene Muncie <nivek2002@gmail.com>
2018-10-22 16:52:13 -0400

    Remove dumb hyphen

--- handler.ts
+++ handler.ts
@@ -1,14 +1,14 @@
 import { APIGatewayEvent, Callback, Context, Handler } from 'aws-lambda';
 
-export const lang-count: Handler = (event: APIGatewayEvent, context: Context, cb: Callback) => {
+export const langCount: Handler = (event: APIGatewayEvent, context: Context, cb: Callback) => {
    const response = {
       statusCode: 200,
       body: JSON.stringify({
-         "uid": "urn:uuid:1335c695-cfb8-4ebb-abbd-80da344efa6b",
-         "updateDate": "2016-05-23T00:00:00.0Z",
-         "titleText": "Amazon Developer Blog, week in review May 23rd",
-         "mainText": "Meet Echosim. A new online community tool for developers that simulates the look and feel of an Amazon Echo.",
-         "redirectionUrl": "https://developer.amazon.com/public/community/blog"
+         uid: 'urn:uuid:1335c695-cfb8-4ebb-abbd-80da344efa6b',
+         updateDate: '2016-05-23T00:00:00.0Z',
+         titleText: 'Amazon Developer Blog, week in review May 23rd',
+         mainText: 'Meet Echosim. A new online community tool for developers that simulates the look and feel of an Amazon Echo.',
+         redirectionUrl: 'https://developer.amazon.com/public/community/blog',
          message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
          input: event,
       }),
